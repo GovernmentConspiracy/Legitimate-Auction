1291f3349ff5214f0eae7429c44bb57e34d931bf
GovernmentConspiracy <liang.j4son@gmail.com>
2021-07-31 03:59:01 -0700

    node dotenv dependencies

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+console.log("Starting...");
+require('dotenv');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN)
+client.on('ready', () => {
+	console.log("Hello, world!");
+})
