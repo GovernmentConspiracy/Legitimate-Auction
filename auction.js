64d4e73e3c4cbf736fe88ea20c35f0547812be1d
GovernmentConspiracy <liang.j4son@gmail.com>
2021-08-02 03:44:45 -0700

    rudimentary bidding

---
+++ auction.js
@@ -0,0 +1,17 @@
+class Auction {
+	seller 			//string
+	item 			//string
+	description 	//string
+	startingBid 	//number
+	increment 		//number
+	buyout 			//number
+
+	/**
+	 * Creates an auction
+	 * @param {number} r 
+	 */
+	constructor(r) {
+		
+	}
+
+}
--- index.js
+++ index.js
@@ -4,12 +4,45 @@ require('dotenv').config();
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
-const startMilli = Date.now()
-
+const startMilli = Date.now();
 console.log("Starting: %s", Date());
-bot.login(process.env.BOT_TOKEN)
-bot.on('ready', () => {
+
+bot.once("ready", () => {
 	console.log("Ready: %dms\n", Date.now() - startMilli);
 	console.log("Hello, world!");
+});
+
+bot.on("message", async (msg) => {
+	
+	console.log(msg)
+	console.log(msg.content)
+
+	
+	if (msg.content === "!deez") {
+		const nutEmoji = '🥜';
+		const message = await msg.channel.send('Who is deez?', { fetchReply: true });
+
+		const filter = (reaction, user) => {
+			return nutEmoji === reaction.emoji.name && user.id != bot.user.id
+		};
+
+		await message.react(nutEmoji)
+			message.awaitReactions(filter, {max: 1, time: 10000, errors: ['time'] })
+				.then((collected) => {
+					const reaction = collected.first();
+					console.log(reaction.users.cache);
+					
+					map = reaction.users.cache;
+					map.delete(bot.user.id);
+					msg.channel.send(`deez nuts ${map.first().toString()}`);
+					
+				})
+				.catch((collected) => {
+					msg.channel.send(`Come on ${msg.author.toString()}, react to the message.`);
+				});
+		
+		
+	}
 })
 
+bot.login(process.env.BOT_TOKEN);
